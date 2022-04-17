183a2e5fc5cba1ff098ee48ff60aaf2d08d333c3
awictor <92066799+awictor@users.noreply.github.com>
2022-04-17 10:39:46 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const menu = {
+  _meal: '',
+  _price: 0,
+
+  set meal(mealToCheck){
+    if(typeof mealToCheck === 'string') {
+      return this._meal = mealToCheck;
+    }
+  },
+
+    set price(priceToCheck) {
+      if (typeof priceToCheck === 'number'){
+        return this._price = priceToCheck;
+      }
+    },
+
+     get todaysSpecial(){
+        if(this._meal && this._price){
+          return `Today's Meal is ${this._meal} for $${this._price}!`
+        } else {
+          return 'Meal or price was not set correctly!'
+        }
+      }
+  
+};
+
+menu.meal = 'Swedish Meatballs'
+menu.price = 17
+console.log(menu)
+
+console.log(menu.todaysSpecial)
+
+
+//set menu._meal() {}
